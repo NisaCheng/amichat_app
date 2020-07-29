060b3fd2ce11f1f532598ab9438ad1bd657fb5e2
NisaCheng <nisacheng@gmail.com>
2020-07-29 14:48:46 -0700

    updating files

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,55 +16,109 @@
 'use strict';
 
 // Signs-in Friendly Chat.
+function signIn() {
   // TODO 1: Sign in Firebase with credential from the Google user.
-
-  function signIn() {
-    // Sign into Firebase using popup auth & Google as the identity provider.
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 }
 
 // Signs-out of Friendly Chat.
-  function signOut() {
-    // Sign out of Firebase.
-    firebase.auth().signOut();
-  
+function signOut() {
+  // TODO 2: Sign out of Firebase.
+  firebase.auth().signOut();
 }
 
 //Initialize firebase.
-function initFirebase(){
-  // TODO
+function initFirebase() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCP2SBduvJiM5WJLOb97dHRpn0UisozXpk",
+    authDomain: "nisa-smapchat.firebaseapp.com",
+    databaseURL: "https://nisa-smapchat.firebaseio.com",
+    projectId: "nisa-smapchat",
+    storageBucket: "nisa-smapchat.appspot.com",
+    messagingSenderId: "740972871349",
+    appId: "1:740972871349:web:17d16b2f08bd6b9d8f88fc",
+    measurementId: "G-3QD3BME9KV"
+  });
 }
+
 // Initiate firebase auth.
 function initFirebaseAuth() {
   // TODO 3: Initialize Firebase.
+  firebase.auth().onAuthStateChanged(authStateObserver);
 }
 
 // Returns the signed-in user's profile Pic URL.
-
-  function getProfilePicUrl() {
-    return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
-  }
+function getProfilePicUrl() {
+  // TODO 4: Return the user's profile pic URL.
+  return (
+    firebase.auth().currentUser.photoURL ||
+    '/images/profile_placeholder.png'
+  );
+}
 
 // Returns the signed-in user's display name.
 function getUserName() {
+  // TODO 5: Return the user's display name.
   return firebase.auth().currentUser.displayName;
 }
 
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
+  // TODO 6: Return true if a user is signed-in.
   return !!firebase.auth().currentUser;
 }
 
 // Saves a new message on the Firebase DB.
 function saveMessage(messageText) {
   // TODO 7: Push a new message to Firebase.
+  /* Example Thing
+    // Add a new document in collection "cities"
+      db.collection("cities").doc("LA").set({
+          name: "Los Angeles",
+          state: "CA",
+          country: "USA"
+      })
+      .then(function() {
+          console.log("Document successfully written!");
+      })
+      .catch(function(error) {
+          console.error("Error writing document: ", error);
+      });
+  */
+  
 }
 
 // Loads chat messages history and listens for upcoming ones.
+// function loadMessages() {
+//   // TODO 8: Load and listens for new messages.
+// }
 function loadMessages() {
-  // TODO 8: Load and listens for new messages.
+  // Create the query to load the last 12 messages and listen for new ones.
+  var query = firebase
+    .firestore()
+    .collection('messages')
+    .orderBy('timestamp', 'desc')
+    .limit(12);
+
+  // Start listening to the query.
+  query.onSnapshot(function (snapshot) {
+    snapshot.docChanges().forEach(function (change) {
+      if (change.type === 'removed') {
+        deleteMessage(change.doc.id);
+      } else {
+        var message = change.doc.data();
+        displayMessage(
+          change.doc.id,
+          message.timestamp,
+          message.name,
+          message.text,
+          message.profilePicUrl,
+          message.imageUrl,
+        );
+      }
+    });
+  });
 }
 
 // Saves a new message containing an image in Firebase.
@@ -95,7 +149,7 @@ function onMediaFileSelected(event) {
   if (!file.type.match('image.*')) {
     var data = {
       message: 'You can only share images',
-      timeout: 2000
+      timeout: 2000,
     };
     signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
     return;
@@ -111,7 +165,7 @@ function onMessageFormSubmit(e) {
   e.preventDefault();
   // Check that the user entered a message and is signed in.
   if (messageInputElement.value && checkSignedInWithMessage()) {
-    saveMessage(messageInputElement.value).then(function() {
+    saveMessage(messageInputElement.value).then(function () {
       // Clear message text field and re-enable the SEND button.
       resetMaterialTextfield(messageInputElement);
       toggleButton();
@@ -119,27 +173,17 @@ function onMessageFormSubmit(e) {
   }
 }
 
-// add event listener on submit button
-
-var btn = document.getElementById("submit");
-btn.addEventListener('click', function() {
-const msg = document.querySelector("#message")
-const msgContainer = document.querySelector("#messages")
-p.innerText = msg.value;
-
-
-}
-)
-
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 function authStateObserver(user) {
-  if (user) { // User is signed in!
+  if (user) {
+    // User is signed in!
     // Get the signed-in user's profile pic and name.
     var profilePicUrl = getProfilePicUrl();
     var userName = getUserName();
 
     // Set the user's profile pic and name.
-    userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userPicElement.style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
     userNameElement.textContent = userName;
 
     // Show user's profile and sign-out button.
@@ -152,7 +196,8 @@ function authStateObserver(user) {
 
     // We save the Firebase Messaging Device token and enable notifications.
     saveMessagingDeviceToken();
-  } else { // User is signed out!
+  } else {
+    // User is signed out!
     // Hide user's profile and sign-out button.
     userNameElement.setAttribute('hidden', 'true');
     userPicElement.setAttribute('hidden', 'true');
@@ -173,7 +218,7 @@ function checkSignedInWithMessage() {
   // Display a message to the user using a Toast.
   var data = {
     message: 'You must sign-in first',
-    timeout: 2000
+    timeout: 2000,
   };
   signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
   return false;
@@ -185,19 +230,20 @@ function resetMaterialTextfield(element) {
   element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
 }
 
-
-
 // Template for messages.
 var MESSAGE_TEMPLATE =
-    '<div class="message-container">' +
-      '<div class="spacing"><div class="pic"></div></div>' +
-      '<div class="message"></div>' +
-      '<div class="name"></div>' +
-    '</div>';
+  '<div class="message-container">' +
+  '<div class="spacing"><div class="pic"></div></div>' +
+  '<div class="message"></div>' +
+  '<div class="name"></div>' +
+  '</div>';
 
 // Adds a size to Google Profile pics URLs.
 function addSizeToGoogleProfilePic(url) {
-  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+  if (
+    url.indexOf('googleusercontent.com') !== -1 &&
+    url.indexOf('?') === -1
+  ) {
     return url + '?sz=150';
   }
   return url;
@@ -234,11 +280,13 @@ function createAndInsertMessage(id, timestamp) {
     let messageListNode = existingMessages[0];
 
     while (messageListNode) {
-      const messageListNodeTime = messageListNode.getAttribute('timestamp');
+      const messageListNodeTime = messageListNode.getAttribute(
+        'timestamp',
+      );
 
       if (!messageListNodeTime) {
         throw new Error(
-          `Child ${messageListNode.id} has no 'timestamp' attribute`
+          `Child ${messageListNode.id} has no 'timestamp' attribute`,
         );
       }
 
@@ -257,23 +305,31 @@ function createAndInsertMessage(id, timestamp) {
 
 // Displays a Message in the UI.
 function displayMessage(id, timestamp, name, text, picUrl, imageUrl) {
-  var div = document.getElementById(id) || createAndInsertMessage(id, timestamp);
+  var div =
+    document.getElementById(id) ||
+    createAndInsertMessage(id, timestamp);
 
   // profile picture
   if (picUrl) {
-    div.querySelector('.pic').style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(picUrl) + ')';
+    div.querySelector('.pic').style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(picUrl) + ')';
   }
 
   div.querySelector('.name').textContent = name;
   var messageElement = div.querySelector('.message');
 
-  if (text) { // If the message is text.
+  if (text) {
+    // If the message is text.
     messageElement.textContent = text;
     // Replace all line breaks by <br>.
-    messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
-  } else if (imageUrl) { // If the message is an image.
+    messageElement.innerHTML = messageElement.innerHTML.replace(
+      /\n/g,
+      '<br>',
+    );
+  } else if (imageUrl) {
+    // If the message is an image.
     var image = document.createElement('img');
-    image.addEventListener('load', function() {
+    image.addEventListener('load', function () {
       messageListElement.scrollTop = messageListElement.scrollHeight;
     });
     image.src = imageUrl + '&' + new Date().getTime();
@@ -281,7 +337,9 @@ function displayMessage(id, timestamp, name, text, picUrl, imageUrl) {
     messageElement.appendChild(image);
   }
   // Show the card fading-in and scroll to view the new message.
-  setTimeout(function() {div.classList.add('visible')}, 1);
+  setTimeout(function () {
+    div.classList.add('visible');
+  }, 1);
   messageListElement.scrollTop = messageListElement.scrollHeight;
   messageInputElement.focus();
 }
@@ -298,17 +356,23 @@ function toggleButton() {
 
 // Checks that the Firebase SDK has been correctly setup and configured.
 function checkSetup() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
+  if (
+    !window.firebase ||
+    !(firebase.app instanceof Function) ||
+    !firebase.app().options
+  ) {
+    window.alert(
+        'You have not configured and imported the Firebase SDK. ' +
         'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
+        'sure you are running the codelab using `firebase serve`',
+    );
   }
 }
 // Shortcuts to DOM Elements.
 var messageListElement = document.getElementById('messages');
 var messageFormElement = document.getElementById('message-form');
 var messageInputElement = document.getElementById('message');
-var submitButtonElement = document.getElementById('submit');
+var submitButtonElement = document.querySelector('#submit');
 var imageButtonElement = document.getElementById('submitImage');
 var imageFormElement = document.getElementById('image-form');
 var mediaCaptureElement = document.getElementById('mediaCapture');
@@ -316,30 +380,26 @@ var userPicElement = document.getElementById('user-pic');
 var userNameElement = document.getElementById('user-name');
 var signInButtonElement = document.getElementById('sign-in');
 var signOutButtonElement = document.getElementById('sign-out');
-var signInSnackbarElement = document.getElementById('must-signin-snackbar');
+var signInSnackbarElement = document.getElementById(
+  'must-signin-snackbar',
+);
 
-submitButtonElement.addEventListener('click', function() {
-  const btnEnable = document.querySelector('#submit'); 
-  btnEnable.disable = false;
-})
+// submitButtonElement.addEventListener('click', function() {
+//   const btnEnable = document.querySelector('#submit');
+//   btnEnable.disable = false;
+//   let p = document.createElement("p");
+//   const msg = document.querySelector('#message')
+//   const msgContainer = document.querySelector('#messages')
 
+//   p.innerText = msg.value
+//   msgContainer.append(p)
+//   msg.value = ""
+//   console.log(msgContainer)
+
+// })
 
 // initialize Firebase
-function initFirebase(){
-  firebase.initializeApp({
-    "apiKey": "AIzaSyDz0Xc2B-BB4Ikux2ne9ABxHZRBQIFHZkg",
-    "authDomain": "friendlychat-2068e.firebaseapp.com",
-    "databaseURL": "https://friendlychat-2068e.firebaseio.com",
-    "projectId": "friendlychat-2068e",
-    "storageBucket": "friendlychat-2068e.appspot.com",
-    "messagingSenderId": "1014510563829",
-    "appId": "1:1014510563829:web:d8648ececd5f6211e6f0bc",
-    "measurementId": "G-LY69RTS6GB"
-  });
-}
-
-
-
+initFirebase();
 // Checks that Firebase has been imported.
 checkSetup();
 
@@ -353,17 +413,14 @@ messageInputElement.addEventListener('keyup', toggleButton);
 messageInputElement.addEventListener('change', toggleButton);
 
 // Events for image upload.
-imageButtonElement.addEventListener('click', function(e) {
+imageButtonElement.addEventListener('click', function (e) {
   e.preventDefault();
   mediaCaptureElement.click();
 });
 mediaCaptureElement.addEventListener('change', onMediaFileSelected);
 
 // initialize Firebase Auth
-function initFirebaseAuth() {
-  // Listen to auth state changes.
-  firebase.auth().onAuthStateChanged(authStateObserver);
-}
+initFirebaseAuth();
 
 // TODO: Enable Firebase Performance Monitoring.
 
